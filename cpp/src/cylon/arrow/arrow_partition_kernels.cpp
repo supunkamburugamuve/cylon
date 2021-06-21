49f817da6725d3fe8776b6f40cea90f4bd4ba341
supunkamburugamuve <supun06@gmail.com>
2021-06-21 12:17:49 -0400

    testing

--- cpp/src/cylon/arrow/arrow_partition_kernels.cpp
+++ cpp/src/cylon/arrow/arrow_partition_kernels.cpp
@@ -66,7 +66,7 @@ template<typename ARROW_T, typename = typename std::enable_if<
         | arrow::is_temporal_type<ARROW_T>::value>::type>
 class ModuloPartitionKernel : public HashPartitionKernel {
   using ARROW_ARRAY_T = typename arrow::TypeTraits<ARROW_T>::ArrayType;
-
+  using T = typename ARROW_T::c_type;
  public:
   Status Partition(const std::shared_ptr<arrow::ChunkedArray> &idx_col,
                    uint32_t num_partitions,
@@ -78,17 +78,19 @@ class ModuloPartitionKernel : public HashPartitionKernel {
     }
 
     Partitioner partitioner = get_partitioner(num_partitions);
-    LoopRunner<ARROW_ARRAY_T> loop_runner = [&](const std::shared_ptr<ARROW_ARRAY_T> &casted_arr,
-                                                uint64_t chunk_offset,
-                                                uint64_t idx) {
-      uint32_t pseudo_hash = 31 * target_partitions[chunk_offset] + static_cast<uint32_t>(casted_arr->Value(idx));
-      uint32_t p = partitioner(pseudo_hash);
-      target_partitions[chunk_offset] = p;
-      partition_histogram[p]++;
-    };
+    uint64_t offset = 0;
+    for (const auto &arr: idx_col->chunks()) {
+      const std::shared_ptr<arrow::ArrayData> &data = arr->data();
+      T *left_data = data->template GetMutableValues<T>(1);
 
-    run_loop(idx_col, loop_runner);
-
+      const std::shared_ptr<ARROW_ARRAY_T> &carr = std::static_pointer_cast<ARROW_ARRAY_T>(arr);
+      for (int64_t i = 0; i < carr->length(); i++, offset++) {
+        uint32_t pseudo_hash = 31 * target_partitions[offset] + static_cast<uint32_t>(left_data[i]);
+        uint32_t p = pseudo_hash % num_partitions;
+        target_partitions[offset] = p;
+        partition_histogram[p]++;
+      }
+    }
     return Status::OK();
   }
 
