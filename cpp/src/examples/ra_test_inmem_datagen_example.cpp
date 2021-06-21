0547773e1b1a19be1edab303a924be2dfbc6f6fd
supunkamburugamuve <supun06@gmail.com>
2021-06-21 12:24:38 -0400

    testing

--- cpp/src/examples/ra_test_inmem_datagen_example.cpp
+++ cpp/src/examples/ra_test_inmem_datagen_example.cpp
@@ -225,17 +225,17 @@ int join_perf(int argc, char **argv) {
             << std::chrono::duration_cast<std::chrono::milliseconds>(join_end_time - read_end_time).count() << "[ms]"
             << " Joined has : " << joined->Rows();
 
-  status = cylon::DistributedJoin(first_table, second_table,
-                                  cylon::join::config::JoinConfig::InnerJoin(0, 0, cylon::join::config::HASH), joined);
-  if (!status.is_ok()) {
-    LOG(INFO) << "Table join failed ";
-    ctx->Finalize();
-    return 1;
-  }
-
-  LOG(INFO) << " Rank " << ctx->GetRank() << " Hash Join done in "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - join_end_time)
-                .count() << "[ms]" << " Joined has : " << joined->Rows();
+//  status = cylon::DistributedJoin(first_table, second_table,
+//                                  cylon::join::config::JoinConfig::InnerJoin(0, 0, cylon::join::config::HASH), joined);
+//  if (!status.is_ok()) {
+//    LOG(INFO) << "Table join failed ";
+//    ctx->Finalize();
+//    return 1;
+//  }
+//
+//  LOG(INFO) << " Rank " << ctx->GetRank() << " Hash Join done in "
+//            << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - join_end_time)
+//                .count() << "[ms]" << " Joined has : " << joined->Rows();
 
   ctx->Finalize();
 
